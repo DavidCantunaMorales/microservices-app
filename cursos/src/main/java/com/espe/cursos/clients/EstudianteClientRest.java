4cb21d2c42890c854a0cde1117f031cac98f091b
David Cantuña <edcantuna@espe.edu.ec>
2025-01-17 11:14:55 -0500

    names microservices, and comunication

--- cursos/src/main/java/com/espe/cursos/clients/EstudianteClientRest.java
+++ cursos/src/main/java/com/espe/cursos/clients/EstudianteClientRest.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "micro_estudiantes", url = "http://localhost:8003/api/estudiantes")
+@FeignClient(name = "estudiantes", url = "http://localhost:8003/api/estudiantes")
 public interface EstudianteClientRest {
     @GetMapping("/{id}")
     Estudiante findById(@PathVariable Long id);
